c5f17dfd3dad4637fa6d115e4a417b8b36fbc05b
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-12-04 17:41:06 -0700

    Route to get 1 visa added

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/visaDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await visaCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/visas", async (req, res) => {
       const newVisa = req.body;
       const result = await visaCollection.insertOne(newVisa);
