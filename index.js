eb26d203d9a5e48cd520515de5a9c362021c6f83
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-12-05 20:03:58 -0700

    readme updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 // middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.85wcl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
